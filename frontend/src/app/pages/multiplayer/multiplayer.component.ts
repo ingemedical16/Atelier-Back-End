b12f040fc08fa1b86dd56718d985f737a6eb46e5
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-09-21 17:49:05 +0200

    angular setup

---
+++ frontend/src/app/pages/multiplayer/multiplayer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiplayer',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiplayer.component.html',
+  styleUrl: './multiplayer.component.css'
+})
+export class MultiplayerComponent {
+
+}
---
+++ frontend/src/app/pages/single-player/single-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-player',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-player.component.html',
+  styleUrl: './single-player.component.css'
+})
+export class SinglePlayerComponent {
+
+}
